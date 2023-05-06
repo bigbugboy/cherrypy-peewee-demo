ef76f667c1b9fd4baeff3801b432f85f3a7ef633
liuxu <liuxu-ynl@foxmail.com>
2023-05-06 15:38:51 +0800

    desc of table question

--- server/models.py
+++ server/models.py
@@ -18,6 +18,23 @@ class People(BaseModel):
     name = CharField()
     age = IntegerField()
 
+"""
+table select_question
+updated_at int(timestamp)
+created_at int(timestamp)
+id int(pk)
+title char
+answer_a char
+answer_b char
+answer_c char
+answer_d char
+correct_answer enum(a/b/c/d)
+topic json(逻辑关联知识点)
+hint char(提示) null
+explain text(解释) null
+hard int(难度系数几颗星)
+
+"""
 
 
 
